2003728a53ac0eb78e68d1e9775c83dc26eece68
woshimayi <zheng_s_e_n@126.com>
2018-07-03 00:36:25 +0800

    问题描述: 23-30
    解决方法:

--- python使用100列/twenty_three.py
+++ python使用100列/twenty_three.py
@@ -8,18 +8,43 @@
 #   ***
 #    *
 
-for i in range(1, 10, 2):
-	j = 0
-	k = 0
-	for k in range( (10-i)/2, i, -1):
+# ======================================================
+# n = 10
+# for i in range(1, n, 2):
+# 	j = 0
+# 	k = 0
+# 	for k in range(int((n-i)/2), 0, -1):
+# 		print(' ',end='')
+# 	for j in range(1, i+1):
+# 		print('*',end='')
+# 	print()
+
+# for i in range(n-2, 1, -2):
+# 	j = 0
+# 	k = 0
+# 	# print(i)
+# 	for j in range(int((n-i)/2), 0, -1):
+# 		print(' ',end='')
+# 	for k in range(1, i):
+# 		print('*',end='')
+# 	print()
+# ======================================================
+
+n = 10
+for i in range(n):
+	for k in range(n-i, 1, -1):
 		print(' ',end='')
-	for j in range(1, i+1):
+	for j in range(2*i-1):
 		print('*',end='')
 	print()
 
-for i in range(11, 1, -2):
-	j = 0
-	k = 0
-	for j in range(i-1, 1, -1):
+for i in range(n):
+	# print(i)
+	for k in range(i+1):
+		print(' ',end='')
+	for j in range((n-i-2)*2, 1, -1):
 		print('*',end='')
 	print()
+# ======================================================
+
+
