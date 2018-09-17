b084bc712d77424f1756d1bd8802864d92cae01d
woshidamayi <woshidamayi@Gmail.com>
2018-09-17 20:38:12 +0800

    问题描述：spin test
    解决方法：

--- GUI/tab_test.py
+++ GUI/tab_test.py
@@ -3,6 +3,7 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+from tkinter import scrolledtext 
 
 win = Tk()
 win.title('Python GUI')
@@ -28,6 +29,63 @@ ttk.Label(monty1, text='Enter a passwd').grid(column=0, row=0, sticky='W')
 chVarDis = tk.IntVar()
 check1 = tk.Checkbutton(monty, text='Disabled', variable=chVarDis, state='disabled')
 check1.grid(column=0, row=1)
+
+def _spin():
+    value = spin.get()
+    print(value)
+    scr.insert(tk.INSERT, value+'\n')
+
+spin = Spinbox(monty, value=(0, 50, 100), width=5, bd=8, command=_spin)
+spin.grid(column=0, row=2)
+
+spin = Spinbox(monty, value=(0, 50, 100), width=5, bd=8, command=_spin)
+spin.grid(column=1, row=2)
+
+class ToolTip(object):
+    def __init__(self,widget):
+        self.widget = widget
+        self.tipwindow = None
+        self.id = None
+        self.x = self.y = 0
+    
+    def showtip(self, text):
+        "Display text in tooltip window"
+        self.text = text
+        if self.tipwindow or not self.text:
+            return
+        x, y, _cn, cy = self.widget.bbox('insert')
+        x = x + self.widget.winfo_rootx() + 27
+        y = y + cy + self.widget.winfo_rooty + 27
+        self.tipwindow = tw = tk,Toplevel(self.widget)
+        tw.wm_overrideredirect(1)
+        tw.wm_geometry("+%d+%d" % (x, y))
+
+        label = tk.Label(tw, text=self.text, justify=tk.LEFT, background = '#ffffe0', relief=tk.SOLID, borderwidth=1, font=("tahome", "8", "normal"))
+        label.pack(ipadx=1)
+    
+    def hidetip(self):
+        tw = self.tipwindow
+        self.tipwindow = None
+        if tw:
+            tw.destroy()
+        
+
+def createToolTip(widget, text):
+    toolTip = ToolTip(widget)
+    def enter(event):
+        toolTip.showtip(text)
+    def leave(event):
+        toolTip.hidetip()
+    widget.bind('<Enter>', enter)
+    widget.bind('<Leave>', leave)
+
+createToolTip(spin, 'This is a Spin control')
+
+scrolW = 30; scrolH = 3
+scr = scrolledtext.ScrolledText(monty, width=scrolW, height=scrolH, wrap=tk.WORD)
+scr.grid(column=0, row=3, stick='WE', columnspan=3)
+
+createToolTip(scr, 'This is a ScrolledText widget')
 
 
 win.mainloop()
