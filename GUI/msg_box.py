617aec5f0831cc56dbb155560f5324ef4f8c798f
woshidamayi <woshidamayi@Gmail.com>
2018-09-18 20:35:33 +0800

    问题描述：messageBox
    解决方法：

--- GUI/msg_box.py
+++ GUI/msg_box.py
@@ -19,17 +19,10 @@ def _quit():
     win.destroy()
     exit()
 
-fileMenu = Menu(menuBar, tearoff=0)  # tearoff 去掉默认的第一个虚线
-fileMenu.add_command(label="New")   # file 选项下拉名称
-fileMenu.add_command(label="Edit")   # file 选项下拉名称
-fileMenu.add_separator() # 添加分割线
-fileMenu.add_command(label='Quit', command=_quit)
-menuBar.add_cascade(label='File', menu=fileMenu)    #创建菜单栏
-
 def _msgBox():
-    # mbox.showinfo('python Message Info Box', 'A Python GUI created')
-    # mbox.showwarning('Python message Warning')
-    # mbox.showerror('Python sdfsdfsdf error')
+    mBox.showinfo('python Message Info Box', 'A Python GUI created')
+    mBox.showwarning('Python message Warning')
+    mBox.showerror('Python sdfsdfsdf error')
     answer = mBox.askyesno("python Message Dual chiise box", 'are you sure you really wish to do this')
 
 helpMenu = Menu(menuBar, tearoff=0)
