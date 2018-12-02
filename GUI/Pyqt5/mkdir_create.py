53e4420ffbb2958a73906d750e25b4274e495b18
woshidamayi <woshidamayi@Gmail.com>
2018-12-02 16:58:49 +0800

    问题描述：修改python工程建立文档
    解决方法：

--- GUI/Pyqt5/mkdir_create.py
+++ GUI/Pyqt5/mkdir_create.py
@@ -2,7 +2,22 @@ import  sys
 import  os
 
 
+str_module = '''# coding=utf-8
 
+
+if __name__ == "__main__":
+	if argc > 2:
+		print(argv[1])
+'''
+
+str_info = '''#########################################################################################################
+     Author: baiyizhanshi
+      Email: 2638288078@qq.com
+create Date: 
+
+
+#########################################################################################################
+'''
 
 
 def MyMkdir(dir):
@@ -22,7 +37,6 @@ def MyMkdir(dir):
 	os.makedirs(dir1)
 	file1 = '%s\%s' % (dir1, dir)
 	FILE = open(file1, 'w')
-	FILE.write('# coding=utf-8')
 	FILE.close()
 
 	dir2 = '%s\%s' % (dir, dir)
@@ -33,33 +47,31 @@ def MyMkdir(dir):
 	file2 = '%s\%s' % (dir3, '__init__.py')
 	print(file2)
 	FILE = open(file2, 'w')
+	FILE.write(str_module)
 	FILE.close()
+
 	file3 = '%s\%s_%s' % (dir3, dir, 'main.py')
 	print(file3)
 	FILE = open(file3, 'w')
-	FILE.write('# coding=utf-8')
+	FILE.write(str_module)
 	FILE.close()
 
 	file4 = '%s\%s' % (dir2, '__init__.py')
 	print(file4)
 	FILE = open(file4, 'w')
-	FILE.write('# coding=utf-8')
-	FILE.write('\r\n\r\n')
-	FILE.write('if __name__ == "__main__":\r\n')
+	FILE.write(str_module)
 	FILE.close()
 	file5 = '%s\%s' % (dir2, 'main.py')
 	print(file5)
 	FILE = open(file5, 'w')
-	FILE.write('# coding=utf-8')
-	FILE.write('\r\n\r\n')
-	FILE.write('if __name__ == "__main__":\r\n')
+	FILE.write(str_module)
 	FILE.close()
 	
 	dir4 = '%s\%s' % (dir, 'docs')
 	os.makedirs(dir4)
 	file6 = '%s\%s' % (dir4, 'conf.py')
 	FILE = open(file6, 'w')
-	FILE.write('# coding=utf-8')
+	FILE.write(str_module)
 	FILE.close()
 	file7 = '%s\%s' % (dir4, 'abc.rst')
 	FILE = open(file7, 'w')
@@ -67,13 +79,13 @@ def MyMkdir(dir):
 
 	file8 = '%s\%s' % (dir, "setup.py")
 	FILE = open(file8, 'w')
-	FILE.write('# coding=utf-8')
-	FILE.write('\r\n\r\n')
-	FILE.write('if __name__ == "__main__":\r\n')
+	FILE.write(str_module)
 	FILE.close()
+
 	file9 = '%s\%s' % (dir, "requirements.txt")
 	FILE = open(file9, 'w')
 	FILE.close()
+
 	file10 = '%s\%s' % (dir, "README")
 	FILE = open(file10, 'w')
 	FILE.close()
