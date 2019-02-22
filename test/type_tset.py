dc830ce8d9baa9d1158811b1913a76d0ec27e8a9
woshidamayi <woshidamayi@Gmail.com>
2019-02-22 21:02:31 +0800

    问题描述：requests
    解决方法：

---
+++ test/type_tset.py
@@ -0,0 +1,11 @@
+c = "sdfsdf"
+i = 45
+f = 45.6534534453453453645657
+s = '''asdasda
+	dfggghf
+xcvxcvxc
+
+'''
+print(s)
+# b= type(f)
+# print(b)
---
+++ test/while_for.py
@@ -0,0 +1,35 @@
+
+
+# i = 10
+# while i > 0:
+# 	i = i - 1 
+# 	print(i)
+# else:
+# 	print("sdfsd")
+
+
+# fives = []
+# for x in range(100):
+# 	if x % 3 == 0:
+# 		fives.append(x)
+# print(fives)
+# 
+
+# cube = lambda x,y : pow((x + y), 3)
+# cube1 = lambda x,y : x + y
+
+# if __name__ == '__main__':
+# 	a = 10
+# 	b = 20
+# 	print('%d' % cube(2, 5))
+# 	print('%d' % cube1(2, 5))
+
+
+
+a = [5, 7, 6, 3, 4, 1, 2]
+b = sorted(a)
+
+L = [('b', 2), ('a' ,1), ('c', 3), ('d', 4)]
+
+sorted(L, cmp=lambda x,y:cmp(x[1],y[1]))
+
