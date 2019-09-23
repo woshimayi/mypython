5a1ab6b0e1d63d9d9872716a948283919c71b254
woshidamayi <woshidamayi@Gmail.com>
2019-09-23 21:03:51 +0800

    问题描述：GUI label pixmap
    解决方法：

--- GUI/Pyqt5/list_widget/Ui_list.py
+++ GUI/Pyqt5/list_widget/Ui_list.py
@@ -17,22 +17,15 @@ class Ui_MainWindow(object):
         self.centralWidget = QtWidgets.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
         self.pushButton = QtWidgets.QPushButton(self.centralWidget)
-        self.pushButton.setGeometry(QtCore.QRect(40, 160, 75, 23))
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 75, 23))
         self.pushButton.setObjectName("pushButton")
         self.lineEdit = QtWidgets.QLineEdit(self.centralWidget)
-        self.lineEdit.setGeometry(QtCore.QRect(130, 160, 551, 20))
+        self.lineEdit.setGeometry(QtCore.QRect(100, 10, 551, 20))
         self.lineEdit.setObjectName("lineEdit")
-
         self.label = QtWidgets.QLabel(self.centralWidget)
-        self.label.setGeometry(QtCore.QRect(80, 280, 521, 81))
+        self.label.setGeometry(QtCore.QRect(160, 90, 0, 0))
         self.label.setObjectName("label")
-
-        self.textview = QtWidgets.QTextBrowser(self.centralWidget)
-        self.textview.setGeometry(QtCore.QRect(200, 300, 520, 81))
-        self.textview.backward()
-        self.label.setObjectName("textview")
-
-
+        self.label.setScaledContents(True)
         MainWindow.setCentralWidget(self.centralWidget)
 
         self.retranslateUi(MainWindow)
@@ -42,8 +35,7 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "PushButton"))
-        self.label.setText(_translate("MainWindow", "aaaaaaaaaaa"))
-
+        self.label.setText(_translate("MainWindow", "aaaaaa"))
 
 
 if __name__ == "__main__":
--- GUI/Pyqt5/list_widget/list.py
+++ GUI/Pyqt5/list_widget/list.py
@@ -6,9 +6,18 @@ Module implementing MainWindow.
 
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit
-import sys
-
+from PyQt5 import QtGui, QtCore
 from Ui_list import Ui_MainWindow
+
+import sys
+from bs4 import BeautifulSoup
+import  requests
+import time
+
+headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36",
+        'authorization': ""
+        }
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     """
@@ -31,10 +40,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         """
         # TODO: not implemented yet
         # raise NotImplementedError
-        print(self.lineEdit.text())
-        self.textview.setText(self.lineEdit.text())
-        self.textview.
-        self.label.setText(self.lineEdit.text())
+        if self.lineEdit.text():
+            print(self.lineEdit.text())
+        
+        # r = requests.get('https://cn.bing.com/', headers=headers)
+        # r.encoding='utf-8'
+        # soup=BeautifulSoup(r.text,"lxml")
+        # img = requests.get('https://cn.bing.com' + soup.link.get('href'))
+        # jpg = str(time.strftime("%Y%m%d%H%M%S", time.localtime())) + '.jpg'
+        # print(jpg)
+        # f = open(jpg, 'wb')
+        # f.write(img.content)
+        # f.close()
+
+        self.pixmap = QtGui.QPixmap("1.jpg")
+        self.label.setGeometry(0, 50, 600, 100)
+
+        self.scaredPixmap = self.pixmap.scaled(self.pixmap.width(), self.pixmap.height(), aspectRatioMode=QtCore.Qt.KeepAspectRatio)
+        # self.label
+        self.label.setPixmap(self.scaredPixmap)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
