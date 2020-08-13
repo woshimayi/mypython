57bc00015a05add7b8daf17e08f12db2c7f0fb2a
woshidamayi <woshidamayi@Gmail.com>
2020-08-13 21:05:35 +0800

    问题描述：邮件接收发送自动化，进行远程控制
    解决方法：

---
+++ mail_socket/pop-recv-email-download-file.py
@@ -0,0 +1,187 @@
+import logging
+import smtplib
+import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.parser import Parser
+from email.header import decode_header, Header
+from email.utils import parseaddr
+import poplib
+
+
+# 设置logging的等级以及打印格式
+# logging.basicConfig(
+#     level=logging.DEBUG,
+# format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s:
+# %(message)s')
+logging.basicConfig(
+    level=logging.ERROR)
+
+
+class Auto_ctrl(object):
+    """docstrinAuto_ctrlssName"""
+
+    def __init__(self):
+        logging.debug('==== recv email ====')
+        # 输入邮件地址, 口令和POP3服务器地址:
+        self.email = 'xxxxxxxxxx@qq.com'
+        self.password = 'xxxxxxxxxxxxxxxx'  # 这个密码不是邮箱登录密码，是pop3服务密码
+        self.pop3_server = 'pop.qq.com'
+        self.email_num = 0
+
+    def decode_str(self, s):
+        value, charset = decode_header(s)[0]
+        if charset:
+            value = value.decode(charset)
+        return value
+
+    def print_info(self, msg):
+        # 输出发件人，收件人，邮件主题信息
+        info = []
+        for header in ['From', 'To', 'Subject']:
+            value = msg.get(header, '')
+            if value:
+                if header == 'Subject':
+                    value = self.decode_str(value)  # 将主题名称解密
+                else:
+                    hdr, addr = parseaddr(value)
+                    name = self.decode_str(hdr)
+                    value = u'%s <%s>' % (name, addr)
+            logging.debug('%s: %s' % (header, value))
+            info.append(value)
+        # logging.debug('===== %s' % info)
+        # 获取邮件主体信息
+        attachment_files = []
+        for part in msg.walk():
+            file_name = part.get_filename()  # 获取附件名称类型
+            contentType = part.get_content_type()  # 获取数据类型
+            mycode = part.get_content_charset()  # 获取编码格式
+            if file_name:
+                h = Header(file_name)
+                dh = decode_header(h)  # 对附件名称进行解码
+                filename = dh[0][0]
+                if dh[0][1]:
+                    filename = self.decode_str(
+                        str(filename, dh[0][1]))  # 将附件名称可读化
+                attachment_files.append(filename)
+                data = part.get_payload(decode=True)  # 下载附件
+                with open(filename, 'wb') as f:  # 在当前目录下创建文件，注意二进制文件需要用wb模式打开
+                    #with open('指定目录路径'+filename, 'wb') as f: 也可以指定下载目录
+                    f.write(data)  # 保存附件
+                logging.debug(f'附件 {filename} 已下载完成')
+            elif contentType == 'text/plain':  # or contentType == 'text/html':
+                # 输出正文 也可以写入文件
+                data = part.get_payload(decode=True)
+                content = data.decode(mycode)
+                logging.debug('正文：%s' % content)
+                logging.debug('附件文件名列表 %s' % attachment_files)
+                info.append(content)
+        return info
+
+    def recv_email(self):
+        # 连接到POP3服务器:
+        server = poplib.POP3_SSL(self.pop3_server, 995)
+        # 可以打开或关闭调试信息:
+        # server.set_debuglevel(1)
+        # 可选:打印POP3服务器的欢迎文字:
+        logging.debug(server.getwelcome().decode('utf-8'))
+        # 身份认证:
+        server.user(self.email)
+        server.pass_(self.password)
+        # stat()返回邮件数量和占用空间:
+        logging.debug('Messages: %s. Size: %s' % server.stat())
+        # list()返回所有邮件的编号:
+        resp, mails, octets = server.list()
+        # 可以查看返回的列表类似[b'1 82923', b'2 2184', ...]
+        logging.debug(mails)
+        # 由于pop3协议不支持对已读未读邮件的标记，因此，要判断一封pop邮箱中的邮件是否是新邮件必须与邮件客户端联合起来才能做到。
+        index = len(mails)
+        logging.error('未读邮件的数量 %s: 已知邮件: %s', index, self.email_num)
+
+        if self.email_num == 0: # 程序初始化
+            self.email_num = index
+            return
+        elif self.email_num < index: # 接受新邮件
+            self.email_num = index
+            pass
+        elif self.email_num == index: #
+            return
+
+        ctrl = []
+        # 获取最新一封邮件, 注意索引号从1开始，最后一个索引代表的是最新接收的邮件:
+        # 可以写个循环，获取所有邮件的内容 for i in range(1,index+1)
+        for i in range(index - 5, index + 1):
+            logging.debug(i)
+            resp, lines, octets = server.retr(i)
+            # logging.debug('===== %s %s %s' % resp, lines, octets)
+            # lines存储了邮件的原始文本的每一行,
+            # 可以获得整个邮件的原始文本:
+            msg_content = b'\r\n'.join(lines).decode('utf-8')
+            # 稍后解析出邮件:
+            msg = Parser().parsestr(msg_content)
+            # 获取邮件内容
+            info = self.print_info(msg)
+            if 'auto' in info:
+                logging.debug("not in list")
+                ctrl = info
+                continue
+            ctrl.clear()
+            # 可以根据邮件索引号直接从服务器删除邮件:
+            # server.dele(2)
+        # 关闭连接:
+        server.quit()
+        return ctrl
+
+    def send_email(self, info):
+        if info is None:
+            logging.error('info is NULL')
+            return
+
+        # sender是邮件发送人邮箱，passWord是服务器授权码，mail_host是服务器地址（这里是QQsmtp服务器）
+        # sender = 'xxxxxxxxxx@qq.com'
+        # passWord = 'xxxxxxxxxxxxxxxx'
+        mail_host = 'smtp.qq.com'
+        # receivers是邮件接收人，用列表保存，可以添加多个
+        receivers = ['xxxxxxxxxx@qq.com']
+
+        # 设置email信息
+        msg = MIMEMultipart()
+        # 邮件主题
+        # msg['Subject'] = input(f"{'请输入邮件主题：'}")
+        msg['Subject'] = str("request")
+        # 发送方信息
+        msg['From'] = self.email
+        # 邮件正文是MIMEText:
+        # msg_content = input(f"{'请输入邮件主内容:'}")
+
+        msg_content = info
+        msg.attach(MIMEText(msg_content, 'plain', 'utf-8'))  # 添加正文
+
+        try:
+            # QQsmtp服务器的端口号为465或587
+            s = smtplib.SMTP_SSL("smtp.qq.com", 465)
+            # s.set_debuglevel(1)
+            s.login(self.email, self.password)
+            # 给receivers列表中的联系人逐个发送邮件
+            for i in range(len(receivers)):
+                to = receivers[i]
+                msg['To'] = to
+                s.sendmail(self.email, to, msg.as_string())
+                logging.debug('Success!', end='')
+            s.quit()
+            logging.debug("All emails have been sent over!", end='')
+        except smtplib.SMTPException as e:
+            logging.debug("Falied,%s", e)
+        # 登录并发送邮件
+
+
+if __name__ == '__main__':
+    A = Auto_ctrl()
+    while True:
+        time.sleep(30)
+        logging.error('sleep 30 secend')
+        ctrl = A.recv_email()
+        if ctrl:
+            logging.error(ctrl)
+            A.send_email("abcdefghijklmnopquvwxyz")
+            ctrl.clear()
---
+++ mail_socket/send-recv-email.py
@@ -0,0 +1,154 @@
+#! /usr/bin/env python
+# coding=utf-8
+import base64
+import sys
+import time
+import poplib
+import smtplib
+
+# 邮件发送函数
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.parser import Parser
+# 用来解析邮件主题
+from email.header import decode_header
+# 用来解析邮件来源
+from email.utils import parseaddr
+
+
+def parser_subject(msg):
+    print(msg)
+    subject = msg['Subject']
+    value, charset = decode_header(subject)[0]
+    if charset:
+        value = value.decode(charset)
+    print('邮件主题： {0}'.format(value))
+    return value
+
+
+def parser_address(msg):
+    hdr, addr = parseaddr(msg['From'])
+    # name 发送人邮箱名称， addr 发送人邮箱地址
+    name, charset = decode_header(hdr)[0]
+    if charset:
+        name = name.decode(charset)
+    print('发送人邮箱名称: {0}，发送人邮箱地址: {1}'.format(name, addr))
+    return addr
+
+
+def parser_content(msg):
+    content = msg.get_payload()
+    # 文本信息
+    content_charset = content[0].get_content_charset()  # 获取编码格式
+    text = content[0].as_string().split('base64')[-1]
+    text_content = base64.b64decode(text).decode(content_charset)  # base64解码
+    print("===============", text_content)
+
+    print("content", len(content))
+    # 添加了HTML代码的信息
+    # content_charset = content[1].get_content_charset()
+    # text = content[1].as_string().split('base64')[-1]
+    # html_content = base64.b64decode(text).decode(content_charset)
+    # print('文本信息: {0}\n添加了HTML代码的信息: {1}'.format(text_content, html_content))
+
+    return text_content
+
+
+def send_email(info):
+    if info is None:
+        return
+
+    # sender是邮件发送人邮箱，passWord是服务器授权码，mail_host是服务器地址（这里是QQsmtp服务器）
+    sender = 'xxxxxxxxxxxx@qq.com'
+    passWord = 'xxxxxxxxxxxxxxxxxx'
+    mail_host = 'smtp.qq.com'
+    # receivers是邮件接收人，用列表保存，可以添加多个
+    receivers = ['xxxxxxxxxxxxxxxx@qq.com']
+
+    # 设置email信息
+    msg = MIMEMultipart()
+    # 邮件主题
+    # msg['Subject'] = input(f"{'请输入邮件主题：'}")
+    msg['Subject'] = str("auto")
+    # 发送方信息
+    msg['From'] = sender
+    # 邮件正文是MIMEText:
+    # msg_content = input(f"{'请输入邮件主内容:'}")
+
+    msg_content = info
+    msg.attach(MIMEText(msg_content, 'plain', 'utf-8'))
+
+    try:
+        # QQsmtp服务器的端口号为465或587
+        s = smtplib.SMTP_SSL("smtp.qq.com", 465)
+        # s.set_debuglevel(1)
+        s.login(sender, passWord)
+        # 给receivers列表中的联系人逐个发送邮件
+        for i in range(len(receivers)):
+            to = receivers[i]
+            msg['To'] = to
+            s.sendmail(sender, to, msg.as_string())
+            print('Success!', end='')
+        s.quit()
+        print("All emails have been sent over!", end='')
+    except smtplib.SMTPException as e:
+        print("Falied,%s", e)
+    # 登录并发送邮件
+
+
+# 邮件接收函数
+def accpet_mail():
+    try:
+        p = poplib.POP3('pop.qq.com')
+
+        p.set_debuglevel(1)
+        # print(p.getwelcome().decode('utf8'))
+        p.user('xxxxxx@qq.com')
+        p.pass_('xxxxxxxxxxx')
+
+        email_num, email_size = p.stat()
+        # print("消息的数量: {0}, 消息的总大小: {1}".format(email_num, email_size))
+
+        # 使用list()返回所有邮件的编号，默认为字节类型的串
+        rsp, msg_list, rsp_siz = p.list()
+        # print(
+        #     "服务器的响应: {0},\n消息列表： {1},\n返回消息的大小： {2}".format(
+        #         rsp, msg_list, rsp_siz))
+        #
+        # print('邮件总数： {}'.format(len(msg_list)))
+
+        # 下面单纯获取最新的一封邮件
+        total_mail_numbers = len(msg_list)
+        rsp, msglines, msgsiz = p.retr(total_mail_numbers)
+        # print("服务器的响应: {0},\n原始邮件内容： {1},\n该封邮件所占字节大小： {2}".format(rsp, msglines, msgsiz))
+
+        msg_content = b'\r\n'.join(msglines).decode('gbk')
+
+        msg = Parser().parsestr(text=msg_content)
+        # print('解码后的邮件信息:\n{}'.format(msg))
+
+        # 关闭与服务器的连接，释放资源
+        p.close()
+        return msg
+
+        # ret = p.stat() #返回一个元组:(邮件数,邮件尺寸)
+        # p.retr('邮件号码') #方法返回一个元组:(状态信息,邮件,邮件尺寸)
+
+    except poplib.error_proto:
+        print("Login failed:")
+        sys.exit(1)
+
+
+# 运行当前文件时，执行sendmail和accpet_mail函数
+if __name__ == "__main__":
+    send_email("aaaaaaaaaaaa")
+    # 返回解码的邮件信息
+    msg = accpet_mail()
+    # 解析邮件主题
+    subject = parser_subject(msg)
+    # 解析发件人详情
+    address = parser_address(msg)
+    # 解析内容
+    content = parser_content(msg)
+
+    print(subject, address, content)
